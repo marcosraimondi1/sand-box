b07f0c556971982a78437dba6bc5bda003d5c9fb
marcosraimondi1 <marcosraimondi1@hotmail.com>
2024-12-10 09:27:15 -0300

    entities, components systems

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,85 @@
+#include "Vector2D.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/Shape.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <SFML/Window/WindowStyle.hpp>
-#include <cstdint>
+#include <SFML/Window/Window.hpp>
 #include <iostream>
+#include <tuple>
+#include <utility>
 
 #define MAX_HEIGHT 500
 #define MAX_WIDTH 500
 
-class Vector2D {
-
+class Component {
   public:
-    float m_x;
-    float m_y;
-
-    Vector2D(float x, float y) : m_x(x), m_y(y) {};
-    Vector2D() : m_x(0), m_y(0) {};
-
-    Vector2D operator+(const Vector2D &vec) {
-        return Vector2D(m_x + vec.m_x, m_y + vec.m_y);
-    };
-
-    Vector2D operator*(float num) { return Vector2D(m_x * num, m_y * num); };
+    bool exists = false; // if entity contains component, defaults to false
 };
 
-class SandGrain {
-    Vector2D m_velocity;
-    Vector2D m_position;
-    Vector2D m_acceleration;
-    static const int m_size = 10;
+class CTransform : public Component {
+  public:
+    Vector2D position = {0, 0};
+    Vector2D velocity = {0, 0};
+    Vector2D acceleration = {0, 0};
+    CTransform() {};
+    CTransform(const Vector2D &p, const Vector2D &v, const Vector2D &a)
+        : position(p), velocity(v), acceleration(a) {};
+};
+
+class CShape : public Component {
+  public:
+    sf::RectangleShape shape;
+    CShape() {};
+};
+
+using ComponentTuple = std::tuple<CTransform, CShape>;
+
+class Entity {
+    ComponentTuple m_components;
+    bool m_alive = true; // if false delete entity
 
   public:
-    SandGrain(Vector2D initialPosition) {
-        m_position = initialPosition;
-        m_acceleration = Vector2D(0, 10 * 100);
+    Entity() {};
+
+    template <typename T, typename... TArgs> T &add(TArgs &&...mArgs) {
+        // TArgs allows for forwarding the constructor parameters
+        auto &component = get<T>();
+        component = T(std::forward<TArgs>(mArgs)...);
+        component.exists = true;
+        return component;
+    };
+    template <typename T> T &get() { return std::get<T>(m_components); };
+    template <typename T> bool has() { return get<T>().exists; };
+    template <typename T> void remove() {
+        get<T>() = T(); // defaults exists to false
+    };
+};
+
+class Systems {
+  public:
+    static void sMovement(std::vector<Entity> &entities, float elapsed) {
+        for (auto &e : entities) {
+            if (!e.has<CTransform>() || !e.has<CShape>())
+                continue;
+            auto &t = e.get<CTransform>();
+            auto s = e.get<CShape>();
+            t.velocity += t.acceleration * elapsed;
+            t.position += t.velocity * elapsed;
+
+            if (t.position.m_y >= MAX_HEIGHT - s.shape.getSize().y)
+                t.position.m_y = MAX_HEIGHT - s.shape.getSize().y;
+        }
     };
 
-    void update(float elapsed) {
-        m_velocity = m_velocity + m_acceleration * elapsed;
-        m_position = m_position + m_velocity * elapsed;
-
-        if (m_position.m_y >= MAX_HEIGHT - m_size)
-            m_position.m_y = MAX_HEIGHT - m_size;
-
-        if (m_position.m_x >= MAX_WIDTH - m_size)
-            m_position.m_x = MAX_WIDTH - m_size;
-
-        if (m_position.m_x <= 0)
-            m_position.m_x = 0;
-    };
-
-    void draw(sf::RenderWindow &window) {
-        sf::RectangleShape shape(sf::Vector2f(m_size, m_size));
-        shape.setPosition(sf::Vector2f(m_position.m_x, m_position.m_y));
-        window.draw(shape);
+    static void sDraw(std::vector<Entity> &entities, sf::RenderWindow &window) {
+        window.clear();
+        for (auto &e : entities) {
+            if (!e.has<CTransform>() || !e.has<CShape>())
+                continue;
+            auto t = e.get<CTransform>();
+            auto s = e.get<CShape>();
+            s.shape.setPosition({t.position.m_x, t.position.m_y});
+            window.draw(s.shape);
+        }
+        window.display();
     };
 };
 
@@ -70,11 +94,10 @@ int main() {
 
     sf::Clock clock;
 
-    std::vector<SandGrain> sand;
-    sand.push_back(SandGrain({0, 0}));
+    Systems sys;
+    std::vector<Entity> entities;
 
     bool isMousePressed = false;
-
     while (window.isOpen()) {
         for (auto event = sf::Event(); window.pollEvent(event);) {
             if (event.type == sf::Event::Closed) {
@@ -88,24 +111,27 @@ int main() {
                     isMousePressed = false;
                 }
             } else if (event.type == sf::Event::MouseMoved) {
-                if (isMousePressed)
-                    sand.push_back(
-                        SandGrain({static_cast<float>(event.mouseMove.x),
-                                   static_cast<float>(event.mouseMove.y)}));
+                if (isMousePressed) {
+                    Entity grain = Entity();
+                    auto &s = grain.add<CShape>();
+                    auto &t = grain.add<CTransform>();
+                    s.shape.setSize({10, 10});
+                    s.shape.setFillColor(
+                        sf::Color(rand() % 255, rand() % 255, rand() % 255));
+                    t.position = Vector2D(event.mouseMove.x, event.mouseMove.y);
+                    t.acceleration = Vector2D(0, 100);
+
+                    entities.push_back(grain);
+                }
             }
         }
 
         sf::Time elapsed = clock.restart();
+
         // Update here
-        for (SandGrain &grain : sand) {
-            grain.update(elapsed.asSeconds());
-        }
-        window.clear();
+        sys.sMovement(entities, elapsed.asSeconds());
 
         // Draw here
-        for (SandGrain &grain : sand) {
-            grain.draw(window);
-        }
-        window.display();
+        sys.sDraw(entities, window);
     }
 }
